46db9caac90e575eff6f63398a29ce3d846590e8
sahithisarvasiddhi <bhavanisahithi.sarvasiddhi@imaginnovate.com>
2023-12-04 18:33:57 +0530

    new end point

---
+++ INTEGRATION_REPORTS/src/main/java/IntegrationReports/IntegrationReportsApplication.java
@@ -0,0 +1,13 @@
+package IntegrationReports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationReportsApplication.class, args);
+	}
+
+}
